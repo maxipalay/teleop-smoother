e47f27b5915d521f3095a045e53bf8c766576a11
Maximiliano Palay <maximilianopalay@gmail.com>
2024-04-09 12:51:45 -0500

    update launchfile to launch plotting node

--- launch/smoother.launch.py
+++ launch/smoother.launch.py
@@ -5,17 +5,36 @@ from launch.substitutions import PathJoinSubstitution
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.parameter_descriptions import ParameterFile
-
+from launch.actions.declare_launch_argument import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration,\
+    PythonExpression
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'plot',
+            default_value='false',
+            description="controls wether the plotting node is launched.\
+                  Values: ['true','false']"),
         Node(
             package='teleop-smoother',
             executable='smoother',
             parameters=[{"linear_acceleration": 0.5,
                          "angular_acceleration":0.5,
                          "input_vel_topic":"cmd_vel",
-                         "output_vel_topic":"cmd_vel_smooth"}]
+                         "output_vel_topic":"cmd_vel_smooth",
+                         "loop_frequency":50.0}]
         ),
+        Node(
+            package='teleop-smoother',
+            executable='plotter',
+            condition=IfCondition(PythonExpression(
+                ['\'', LaunchConfiguration('plot'), '\'', '==\'true\''])),
+            parameters=[{"raw_vel_topic":"cmd_vel",
+                         "smooth_vel_topic":"cmd_vel_smooth",
+                         "loop_frequency":50.0
+                         }]
+        )
         
     ])
